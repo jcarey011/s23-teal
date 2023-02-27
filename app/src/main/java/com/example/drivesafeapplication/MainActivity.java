47232a6cc8885fad2129c4565feceeb9b93a2fc3
Jasmine C <jcarey011@yahoo.com>
2023-02-27 17:54:46 -0500

    editing main to add register button and backend code

--- app/src/main/java/com/example/drivesafeapplication/MainActivity.java
+++ app/src/main/java/com/example/drivesafeapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.drivesafeapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -17,20 +18,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView username =(TextView) findViewById(R.id.username);
-        TextView password =(TextView) findViewById(R.id.password);
+        TextView username = (TextView) findViewById(R.id.username);
+        TextView password = (TextView) findViewById(R.id.password);
 
-        MaterialButton loginbtn = (MaterialButton)  findViewById(R.id.loginbtn);
+        MaterialButton loginbtn = (MaterialButton) findViewById(R.id.loginbtn);
+        MaterialButton createbtn = (MaterialButton) findViewById(R.id.createbtn);
 
-    loginbtn.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin")) {
-                Toast.makeText(MainActivity.this, "LOGIN SUCCESSFUL", Toast.LENGTH_SHORT).show();
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (username.getText().toString().equals("admin") && password.getText().toString().equals("admin")) {
+                    Toast.makeText(MainActivity.this, "LOGIN SUCCESSFUL", Toast.LENGTH_SHORT).show();
+                } else
+                    Toast.makeText(MainActivity.this, "LOGIN Failed", Toast.LENGTH_SHORT).show();
             }
-            else
-                Toast.makeText(MainActivity.this, "LOGIN Failed", Toast.LENGTH_SHORT).show();
-        }
-    });
+        });
+
+        createbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, SignUpActivity.class));
+            }
+        });
     }
 }
