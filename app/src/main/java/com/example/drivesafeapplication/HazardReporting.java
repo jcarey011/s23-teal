9e6fadafc49b64912f804b86cbf9b163cd3f3ab5
Jasmine C <jcarey011@yahoo.com>
2023-02-27 19:24:33 -0500

    adding hazard reporting

---
+++ app/src/main/java/com/example/drivesafeapplication/HazardReporting.java
@@ -0,0 +1,14 @@
+package com.example.drivesafeapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HazardReporting extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hazard_reporting);
+    }
+}
