4fe33b29ad13c131f1a58581d404fd246229675c
DhruvParikh1 <98427247+DhruvParikh1@users.noreply.github.com>
2023-04-11 11:59:07 -0400

    fixed logical errors preventing code from building

--- drivesafe_app/src/drivesafe_desktop.java
+++ drivesafe_app/src/drivesafe_desktop.java
@@ -12,7 +12,7 @@ public class drivesafe_desktop {
     public drivesafe_desktop() {
         try {
             // Create a connection to your database
-            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/drive_safe", "root", "");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/drive_safe", "root", "root");
 
             // Create a statement object to execute SQL statements
             stmt = conn.createStatement();
@@ -22,72 +22,71 @@ public class drivesafe_desktop {
     }
 
     public static void main(String[] args) {
-        drivesafe_application main = new drivesafe_application();
+        drivesafe_desktop main = new drivesafe_desktop();
         JFrame frame = new JFrame("DriveSafe Data Query Interface");
-    
+
         // Create a text field where the user can enter their SQL statement
         JTextField sqlTextField = new JTextField();
         sqlTextField.setPreferredSize(new Dimension(400, 20));
-    
-// Create a button that the user can click to execute the SQL statement
-JButton queryButton = new JButton("Query");
-queryButton.addActionListener(new ActionListener() {
-    public void actionPerformed(ActionEvent e) {
-        try {
-            // Get the SQL statement from the text field
-            String sql = sqlTextField.getText();
 
-            // Execute the SQL statement
-            ResultSet rs = main.stmt.executeQuery(sql);
+        // Create a button that the user can click to execute the SQL statement
+        JButton queryButton = new JButton("Query");
+        queryButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    // Get the SQL statement from the text field
+                    String sql = sqlTextField.getText();
 
-            // Display the results in a new window
-            DefaultTableModel tableModel = new DefaultTableModel() {
-                public boolean isCellEditable(int row, int column) {
-                    return false;
+                    // Execute the SQL statement
+                    ResultSet rs = main.stmt.executeQuery(sql);
+
+                    // Display the results in a new window
+                    DefaultTableModel tableModel = new DefaultTableModel() {
+                        public boolean isCellEditable(int row, int column) {
+                            return false;
+                        }
+                    };
+                    JTable table = new JTable(tableModel);
+                    JScrollPane scrollPane = new JScrollPane(table);
+                    frame.getContentPane().add(scrollPane, BorderLayout.CENTER);
+                    frame.pack();
+                    frame.setVisible(true);
+
+                    // Fill the table model with data from the result set
+                    ResultSetMetaData metaData = rs.getMetaData();
+                    int columnCount = metaData.getColumnCount();
+                    for (int i = 1; i <= columnCount; i++) {
+                        tableModel.addColumn(metaData.getColumnLabel(i));
+                    }
+                    while (rs.next()) {
+                        Object[] rowData = new Object[columnCount];
+                        for (int i = 1; i <= columnCount; i++) {
+                            rowData[i - 1] = rs.getObject(i);
+                        }
+                        tableModel.addRow(rowData);
+                    }
+                } catch (SQLException ex) {
+                    ex.printStackTrace();
                 }
-            };
-            JTable table = new JTable(tableModel);
-            JScrollPane scrollPane = new JScrollPane(table);
-            frame.getContentPane().add(scrollPane, BorderLayout.CENTER);
-            frame.pack();
-            frame.setVisible(true);
+            }
+        });
 
-            // Fill the table model with data from the result set
-            ResultSetMetaData metaData = rs.getMetaData();
-            int columnCount = metaData.getColumnCount();
-            for (int i = 1; i <= columnCount; i++) {
-                tableModel.addColumn(metaData.getColumnLabel(i));
-            }
-            while (rs.next()) {
-                Object[] rowData = new Object[columnCount];
-                for (int i = 1; i <= columnCount; i++) {
-                    rowData[i - 1] = rs.getObject(i);
-                }
-                tableModel.addRow(rowData);
-            }
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-        }
-    }
-});
-    
         // Add the text field and button to the frame
         JPanel panel = new JPanel();
         panel.add(sqlTextField);
         panel.add(queryButton);
         frame.getContentPane().add(panel, BorderLayout.NORTH);
-    
+
         // Set the size of the frame to cover 60% of the screen
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         int width = (int) (screenSize.width * 0.6);
         int height = (int) (screenSize.height * 0.6);
         frame.setSize(new Dimension(width, height));
-    
+
         // Center the frame on the screen
         frame.setLocationRelativeTo(null);
-    
+
         frame.setVisible(true);
     }
-    
-    
+
 }
