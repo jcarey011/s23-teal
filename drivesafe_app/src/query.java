c3676b7fde7e2af15627b9b1544fc44c3abc5666
DhruvParikh1 <98427247+DhruvParikh1@users.noreply.github.com>
2023-04-30 12:00:50 -0400

    Query panel UI update still a couple things needed to fix (tables position)

--- drivesafe_app/src/query.java
+++ drivesafe_app/src/query.java
@@ -1,27 +1,21 @@
 import java.sql.*;
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
-
 import java.awt.*;
 import java.awt.event.*;
 import java.io.IOException;
-import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import org.json.JSONArray;
-import org.json.JSONObject;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.net.URL;
 import java.io.File;
 import java.io.FileWriter;
 
-
-
 public class query {
-String sql;
+    String sql;
+
     private JSONArray fetchLocations(String sqlcopy) {
         JSONArray locations = new JSONArray();
         try {
@@ -47,7 +41,6 @@ String sql;
                 template = template.replace("/*LOCATIONS_JSON*/", locations.toString());
 
                 // Generate map.html in the same directory as map_template.html
-                Path outputPath = templatePath.resolveSibling("map.html");
                 FileWriter fileWriter = new FileWriter("map.html");
                 fileWriter.write(template);
                 fileWriter.close();
@@ -67,8 +60,6 @@ String sql;
     }
 
     private void initialize() {
-  
-
         try {
             // Create a connection to your database
             conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/tealtest", "root", "root");
@@ -79,25 +70,154 @@ String sql;
             e.printStackTrace();
         }
 
-
-    
         JFrame frame = new JFrame("DriveSafe Data Query Interface");
-        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); // Fix for the main issue
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.setBounds(100, 100, 600, 400);
+        frame.getContentPane().setLayout(new BorderLayout(10, 10));
 
-        // Create a text field where the user can enter their SQL statement
-        JTextField sqlTextField = new JTextField();
-        sqlTextField.setPreferredSize(new Dimension(400, 20));
+        JLabel titleLabel = new JLabel("DriveSafe Query");
+        titleLabel.setFont(new Font("Tahoma", Font.BOLD, 36));
+        titleLabel.setForeground(Color.decode("#333333"));
+        titleLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        frame.getContentPane().add(titleLabel, BorderLayout.NORTH);
+
+        JPanel panel = new JPanel();
+        panel.setBackground(Color.WHITE);
+        panel.setLayout(new BorderLayout(10, 10));
+
+        GridBagConstraints c = new GridBagConstraints();
+
+        // Input panel for SELECT text, input box, Query button, and Show Map button
+        JPanel inputPanel = new JPanel();
+        inputPanel.setBackground(Color.WHITE);
+        inputPanel.setLayout(new GridBagLayout());
+
+        // Add the SELECT text field
+        JTextField sqlTextField = new JTextField("SELECT");
+        sqlTextField.setPreferredSize(new Dimension(70, 20));
+        sqlTextField.setEditable(false);
+        sqlTextField.setFont(new Font("Tahoma", Font.PLAIN, 16));
+        sqlTextField.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 0));
+
+        c.gridx = 0;
+        c.gridy = 0;
+        c.gridwidth = 1;
+        c.gridheight = 1;
+        c.anchor = GridBagConstraints.WEST;
+        c.insets = new Insets(0, 20, 10, 0);
+        inputPanel.add(sqlTextField, c);
+
+        // Add the editable SQL text field
+        JTextField editableSqlTextField = new JTextField();
+        editableSqlTextField.setPreferredSize(new Dimension(400, 20));
+        editableSqlTextField.setFont(new Font("Tahoma", Font.PLAIN, 16));
+        editableSqlTextField.requestFocus();
+
+        c.gridx = 1;
+        c.gridy = 0;
+        c.gridwidth = 2;
+        c.gridheight = 1;
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.insets = new Insets(0, 5, 10, 20);
+        inputPanel.add(editableSqlTextField, c);
 
         // Create a button that the user can click to execute the SQL statement
         JButton queryButton = new JButton("Query");
+        queryButton.setFont(new Font("Tahoma", Font.BOLD, 16));
+        queryButton.setBackground(Color.decode("#28A745"));
+        queryButton.setForeground(Color.WHITE);
+        queryButton.setFocusPainted(false);
+        queryButton.setBorder(BorderFactory.createEmptyBorder(10, 20, 10, 20));
+
+        c.gridx = 3;
+        c.gridy = 0;
+        c.gridwidth = 1;
+        c.gridheight = 1;
+        c.fill = GridBagConstraints.NONE;
+        c.anchor = GridBagConstraints.WEST;
+        c.insets = new Insets(0, 20, 20, 10); // for the "Query" button
+        inputPanel.add(queryButton, c);
+
+        JButton showMapButton = new JButton("Show Map");
+        showMapButton.setFont(new Font("Tahoma", Font.BOLD, 16));
+        showMapButton.setBackground(Color.decode("#007BFF"));
+        showMapButton.setForeground(Color.WHITE);
+        showMapButton.setFocusPainted(false);
+        showMapButton.setBorder(BorderFactory.createEmptyBorder(10, 20, 10, 20));
+
+        c.gridx = 4;
+        c.gridy = 0;
+        c.gridwidth = 1;
+        c.gridheight = 1;
+        c.fill = GridBagConstraints.NONE;
+        c.anchor = GridBagConstraints.WEST;
+        c.insets = new Insets(0, 10, 20, 20); // for the "Show Map" button
+        inputPanel.add(showMapButton, c);
+
+        panel.add(inputPanel, BorderLayout.NORTH);
+
+        // Scroll panel for tables list
+        JPanel scrollPanel = new JPanel();
+        scrollPanel.setBackground(Color.WHITE);
+        scrollPanel.setLayout(new GridBagLayout());
+
+        // Add the tables list
+        JLabel tablesLabel = new JLabel("TABLES");
+        tablesLabel.setFont(new Font("Tahoma", Font.BOLD, 16));
+        tablesLabel.setForeground(Color.decode("#333333"));
+
+        c.gridx = 0;
+        c.gridy = 0;
+        c.gridwidth = 1;
+        c.gridheight = 1;
+        c.fill = GridBagConstraints.NONE;
+        c.anchor = GridBagConstraints.CENTER;
+        c.insets = new Insets(0, 0, 10, 20);
+        scrollPanel.add(tablesLabel, c);
+
+        DefaultListModel<String> tablesListModel = new DefaultListModel<>();
+        try {
+            DatabaseMetaData dbMetaData = conn.getMetaData();
+            String[] types = {"TABLE"};
+            ResultSet rs = dbMetaData.getTables("tealtest", null, "%", types);
+            while (rs.next()) {
+                tablesListModel.addElement(rs.getString("TABLE_NAME"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        
+        JList<String> tablesList = new JList<>(tablesListModel);
+        JScrollPane tablesListScrollPane = new JScrollPane(tablesList);
+        tablesListScrollPane.setPreferredSize(new Dimension(150, 150));
+
+        c.gridx = 0;
+        c.gridy = 1;
+        c.gridwidth = 1;
+        c.gridheight = 2;
+        c.anchor = GridBagConstraints.NORTH;
+        c.insets = new Insets(0, 0, 20, 20);
+        scrollPanel.add(tablesListScrollPane, c);
+
+        panel.add(scrollPanel, BorderLayout.WEST);
+
+        JPanel resultsPanel = new JPanel();
+        resultsPanel.setBackground(Color.WHITE);
+        resultsPanel.setLayout(new BorderLayout());
+        panel.add(resultsPanel, BorderLayout.CENTER);
+
         queryButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 try {
                     // Get the SQL statement from the text field
-                    sql = sqlTextField.getText();
+                    sql = "SELECT " + editableSqlTextField.getText();
 
                     // Execute the SQL statement
                     ResultSet rs = stmt.executeQuery(sql);
+
+                    // Hide tables information
+                    tablesLabel.setVisible(false);
+                    tablesListScrollPane.setVisible(false);
 
                     // Display the results in a new window
                     DefaultTableModel tableModel = new DefaultTableModel() {
@@ -107,11 +227,18 @@ String sql;
                     };
                     JTable table = new JTable(tableModel);
                     JScrollPane scrollPane = new JScrollPane(table);
-                    frame.getContentPane().add(scrollPane, BorderLayout.CENTER);
-                    frame.pack();
-                    frame.setVisible(true);
-                    
-                    // Fill the table model with data from the result set
+
+                    resultsPanel.removeAll();
+
+                    resultsPanel.add(scrollPane, BorderLayout.CENTER);
+
+                    SwingUtilities.invokeLater(new Runnable() {
+                        public void run() {
+                            frame.pack();
+                            frame.setVisible(true);
+                        }
+                    });
+
                     ResultSetMetaData metaData = rs.getMetaData();
                     int columnCount = metaData.getColumnCount();
                     for (int i = 1; i <= columnCount; i++) {
@@ -130,7 +257,6 @@ String sql;
             }
         });
 
-        JButton showMapButton = new JButton("Show Map");
         showMapButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 JSONArray locations = fetchLocations(sql);
@@ -143,12 +269,7 @@ String sql;
             }
         });
 
-        // Add the text field and button to the frame
-        JPanel panel = new JPanel();
-        panel.add(sqlTextField);
-        panel.add(queryButton);
-        panel.add(showMapButton);
-        frame.getContentPane().add(panel, BorderLayout.NORTH);
+        frame.getContentPane().add(panel, BorderLayout.CENTER);
 
         // Set the size of the frame to cover 60% of the screen
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
